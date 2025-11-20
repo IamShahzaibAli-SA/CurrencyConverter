8855820e5af389873bbaf887eccae7bbba5149e5
IamShahzaibAli-SA <243137271+IamShahzaibAli-SA@users.noreply.github.com>
2025-11-20 02:51:50 -0800

    Arrows Hidden

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,19 @@ function App() {
 
   return (
     <>
-      <div></div>
+      <div className='flex justify-center'>
+        <div className='w-[700px] p-5 bg-gray-500'>
+          <div className=''>
+            <p className='text-white text-[30px]'>Currency Converter</p>
+          </div>
+          <div className=''>
+            <p className='text-white text-[20px]'>Enter Amount</p>
+            <input 
+            className='bg-white text-black'
+            type="number" />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
